f1a765f552bf6eefb64943c8c3c0adb9f22322c6
yuri-kiss <135030944+yuri-kiss@users.noreply.github.com>
2024-11-07 08:29:37 -0500

    RIP that wont work :( ima make a fork

--- src/containers/tw-paint-editor-wrapper.jsx
+++ src/containers/tw-paint-editor-wrapper.jsx
@@ -3,25 +3,12 @@ import PropTypes from 'prop-types';
 import PaintEditor from './paint-editor.jsx';
 import {connect} from 'react-redux';
 import {resetZoomLevels} from '../reducers/zoom-levels.js';
-// todo: use createRoot as this is a depricated function
-import {render} from 'react-dom';
 
 // PaintEditor currently can not handle dynamically changing width and height for various nontrivial reasons
 // However, we can work around that by creating a new PaintEditor whenever the width or height changes,
 // which does work. That's what this does.
 
 class TWPaintEditorWrapper extends React.Component {
-    static inject (domNode, props) {
-        props.onResetZoomLevels ??= (() => (void 0));
-        render(
-            <TWPaintEditorWrapper
-                width={props.width || 480}
-                height={props.height || 360}
-                onResetZoomLevels={props.onResetZoomLevels}
-            />,
-            domNode
-        );
-    }
     constructor (props) {
         super(props);
         this.state = {
